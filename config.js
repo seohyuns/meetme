df8a570ea84a8b2c4a929b43a220a62277225ae7
Clara Shim <seohyuns@andrew.cmu.edu>
2014-12-10 22:04:24 -0500

    database for deployment

--- config.js
+++ config.js
@@ -1,3 +1,16 @@
+// default to a 'localhost' configuration:
+var connection_string = '127.0.0.1:27017/meetme';
+// if OPENSHIFT env variables are present, use the available connection info:
+if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD){
+  connection_string = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+  process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+  process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+  process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+  process.env.OPENSHIFT_APP_NAME;
+}
+
+
 module.exports = {
-	db : 'mongodb://localhost:27017/meetme'
+
+	db : 'mongodb://' + connection_string
 }
